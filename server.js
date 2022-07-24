9c8250a2f06bf7215ceaff0c69781a576103c3c4
cameronstroup <90347622+cameronstroup@users.noreply.github.com>
2022-07-23 19:48:49 -0500

    added article route

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("lets do this");
+const express = require("express");
+const articleRouter = require("./routes/articles");
+const app = express();
+app.set("view engine", "ejs");
+
+app.use("/articles", articleRouter);
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(5000);
